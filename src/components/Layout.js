931db538a4ea63d9183e3b6a7c16976314801df5
annatsu <brendonpagano@gmail.com>
2019-10-16 20:33:25 -0300

    WIP Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,24 @@
+// Node Modules
+import React from "react"
+import styled from "@emotion/styled"
+
+// Components
+import Navbar from "./Navbar"
+
+// Styles
+import "normalize.css"
+import "./Layout.css"
+import { GLOBAL_PADDING, NAVBAR_HEIGHT } from "../constants/styles"
+
+const Wrapper = styled.div`
+  padding-top: ${NAVBAR_HEIGHT + GLOBAL_PADDING}px;
+`
+
+const Layout = ({ children }) => (
+  <Wrapper>
+    <Navbar />
+    {children}
+  </Wrapper>
+)
+
+export default Layout
