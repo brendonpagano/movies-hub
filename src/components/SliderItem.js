31b132488c56109d2a716ea4627b8e4d91aabcbc
annatsu <brendonpagano@gmail.com>
2019-10-17 18:45:17 -0300

    Add class methods to SliderItem in order to detect hover events

--- src/components/SliderItem.js
+++ src/components/SliderItem.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "@emotion/styled"
 
 const ItemPoster = styled.img`
@@ -14,10 +14,56 @@ const ListItem = styled.li`
   }
 `
 
-const SliderItem = ({ title, posterUrl }) => (
-  <ListItem>
-    <ItemPoster src={posterUrl} alt={title} />
-  </ListItem>
-)
+class SliderItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isActive: false,
+    }
+
+    this.mouseInside = false
+    this.activationThrottlerId = null
+  }
+
+  componentWillUnmount() {
+    this.killThrottlerFunction()
+  }
+
+  killThrottlerFunction = () => {
+    if (this.activationThrottlerId) {
+      window.clearTimeout(this.activationThrottlerId)
+      this.activationThrottlerId = null
+    }
+  }
+
+  onMouseEnter = () => {
+    this.killThrottlerFunction()
+
+    this.activationThrottlerId = window.setTimeout(() => {
+      this.setState({ isActive: true })
+    }, 1000)
+  }
+
+  onMouseLeave = () => {
+    this.killThrottlerFunction()
+    if (this.state.isActive) {
+      this.setState({ isActive: false })
+    }
+  }
+
+  render() {
+    const { title, posterUrl } = this.props
+
+    return (
+      <ListItem
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <ItemPoster src={posterUrl} alt={title} />
+      </ListItem>
+    )
+  }
+}
 
 export default SliderItem
