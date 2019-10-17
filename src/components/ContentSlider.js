73310630f825d0bfb1dcc380c9272288047531ed
annatsu <brendonpagano@gmail.com>
2019-10-17 18:19:57 -0300

    Add ContentSlider component

---
+++ src/components/ContentSlider.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+const ContentSlider = styled.ul`
+  list-style-type: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+
+export default ContentSlider
