6de1e8e3dd94212c21b8adf59ce17d9f51572274
annatsu <brendonpagano@gmail.com>
2019-10-17 18:20:18 -0300

    Add Section and SectionTitle component

---
+++ src/components/Section.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Section = ({ children }) => <div>{children}</div>
+
+export default Section
---
+++ src/components/SectionTitle.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+
+const SectionTitle = styled.h3`
+  color: #e5e5e5;
+  font-size: 16px;
+  margin-top: 0;
+`
+
+export default SectionTitle
