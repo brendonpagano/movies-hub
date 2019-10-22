4a98dd938d92c18f3b02f4e72a3c8be1d41d76af
annatsu <brendonpagano@gmail.com>
2019-10-22 16:50:26 -0300

    Remove SectionTitle margin and use relative font sizing

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -2,8 +2,8 @@ import styled from "@emotion/styled"
 
 const SectionTitle = styled.h3`
   color: #e5e5e5;
-  font-size: 16px;
-  margin-top: 0;
+  font-size: 1.2rem;
+  margin: 0;
 `
 
 export default SectionTitle
