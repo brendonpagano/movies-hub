fa139441ad2e8f1c8ad2692caffec3f9689e0899
annatsu <brendonpagano@gmail.com>
2019-10-27 12:12:00 -0300

    Implement ControllableApp component

--- src/components/ControllableApp.js
+++ src/components/ControllableApp.js
@@ -1,49 +1,62 @@
 // Node Modules
 import React, { useState, createContext, useEffect } from "react"
 
-// Custom Hooks
-import useKeyboard from "../hooks/useKeyboard"
+const keyActions = {
+  ArrowUp: ([row, col]) => [--row, col],
+  ArrowDown: ([row, col]) => [++row, col],
+  ArrowLeft: ([row, col]) => [row, --col],
+  ArrowRight: ([row, col]) => [row, ++col],
+  Esc: () => [],
 
-// Constants
-const KEYS_TO_LISTEN = [
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "Enter",
-  "Esc",
-  "Backspace",
-]
+  // We use `13` instead of `Enter` since some Android controllers have a
+  // key property as `Unidentified` but still has the keyCode to 13 when
+  // pressing the OK button.
+  13: () => [],
+}
 
 const ControllerContext = createContext()
 
 const ControllableApp = ({ children }) => {
-  const pressedKey = useKeyboard()
+  const [controllerPosition, setControllerPosition] = useState([0, 0])
 
-  const contextValue = {}
+  useEffect(() => {
+    function onKeyUp(e) {
+      const { key, keyCode } = e
+      let f
+
+      if (keyActions.hasOwnProperty(key)) {
+        f = keyActions[key]
+      } else if (keyActions.hasOwnProperty(keyCode)) {
+        f = keyActions[keyCode]
+      } else {
+        return
+      }
+
+      const newControllerPosition = f(controllerPosition)
+
+      if (
+        newControllerPosition[0] !== controllerPosition[0] ||
+        newControllerPosition[1] !== controllerPosition[1]
+      ) {
+        setControllerPosition(newControllerPosition)
+      }
+    }
+
+    window.addEventListener("keyup", onKeyUp)
+    return () => {
+      window.removeEventListener("keyup", onKeyUp)
+    }
+  })
+
+  const contextValue = {
+    controllerPosition,
+  }
 
   return (
     <ControllerContext.Provider value={contextValue}>
       {children}
     </ControllerContext.Provider>
   )
-}
-
-const getNewControllerPosition = (key, activeControllerPos) => {
-  const [row, col] = activeControllerPos
-
-  switch (key) {
-    case "ArrowUp":
-      return [--row, col]
-    case "ArrowDown":
-      return [++row, col]
-    case "ArrowLeft":
-      return [row, --col]
-    case "ArrowRight":
-      return [row, ++col]
-    default:
-      return activeControllerPos
-  }
 }
 
 export default ControllableApp
